4c756ff3a29f2ecda996fd1fe6eeb0412fb273f6
Shivani Agrawal <agrawal.shivanifxbytes.com>
2018-11-01 14:56:59 +0530

    create order and brand table

---
+++ database/migrations/2018_11_01_092008_create_brand_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brand', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('product_id');
+            $table->string('brand_name');
+            $table->smallInteger('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brand');
+    }
+}
