ee182c71c91cbc05d5b2098f49ed3a91e0df55ec
shriyafxbytes <jain.shriya@fxbytes.com>
2018-11-02 10:18:14 +0530

    some route changes

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,9 @@ Route::get('/product', '\App\Modules\Product\Controllers\ProductController@index
 Route::get('/product/create', '\App\Modules\Product\Controllers\ProductController@create')->name('product.create')->middleware('auth');
 Route::get('/product/delete/{id}', '\App\Modules\Product\Controllers\ProductController@destroy')->name('product.destroy')->middleware('auth');
 Route::get('/product/show/{id}', '\App\Modules\Product\Controllers\ProductController@show')->name('product.show')->middleware('auth');
-
 Route::post('/product', '\App\Modules\Product\Controllers\ProductController@store')->name('product.store')->middleware('auth');
-//category route
 
+//category route
 Route::get('/category', '\App\Modules\Category\Controllers\CategoryController@index')->name('category.index')->middleware('auth');
 Route::get('/addCategory/{category_id?}', '\App\Modules\Category\Controllers\CategoryController@getCategory')->name('addCategory')->middleware('auth');
 Route::post('/addCategory/{category_id?}', '\App\Modules\Category\Controllers\CategoryController@postCategory')->middleware('auth');
@@ -35,9 +34,11 @@ Route::get('category/show/{id}', '\App\Modules\Category\Controllers\CategoryCont
 //payment method route
 Route::get('/payment_method', '\App\Modules\PaymentMethod\Controllers\PaymentMethodController@index')->name('payment_method.index')->middleware('auth');
 Route::get('/payment_method/create', '\App\Modules\PaymentMethod\Controllers\PaymentMethodController@create')->name('payment_method.create')->middleware('auth');
-Route::get('/payment_method/{id}', '\App\Modules\PaymentMethod\Controllers\PaymentMethodController@show')->name('payment_method.show')->middleware('auth');
+Route::get('/payment_method/show/{id}', '\App\Modules\PaymentMethod\Controllers\PaymentMethodController@show')->name('payment_method.show')->middleware('auth');
+
 //order method route
 Route::get('/order', '\App\Modules\Order\Controllers\OrderController@index')->name('order.index')->middleware('auth');
+
 //brand method route
 Route::get('/brand', '\App\Modules\Brand\Controllers\BrandController@index')->name('brand.index')->middleware('auth');
 Route::get('/addBrand/{brand_id?}', '\App\Modules\Brand\Controllers\BrandController@getBrand');
