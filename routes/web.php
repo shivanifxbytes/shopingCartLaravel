1cbda8d881bea0f654d002307318b5f1410e7c9b
shriyafxbytes <jain.shriya@fxbytes.com>
2018-11-02 10:15:11 +0530

    some route changes

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user/verify/{token}', 'Auth\RegisterController@verifyUser');
 //product route
 Route::get('/product', '\App\Modules\Product\Controllers\ProductController@index')->name('product.index')->middleware('auth');
-Route::get('/product/create', '\App\Modules\Product\Controllers\ProductController@getCategory')->name('product.create')->middleware('auth');
+Route::get('/product/create', '\App\Modules\Product\Controllers\ProductController@create')->name('product.create')->middleware('auth');
 Route::get('/product/delete/{id}', '\App\Modules\Product\Controllers\ProductController@destroy')->name('product.destroy')->middleware('auth');
 Route::get('/product/show/{id}', '\App\Modules\Product\Controllers\ProductController@show')->name('product.show')->middleware('auth');
 
@@ -29,11 +29,13 @@ Route::post('/product', '\App\Modules\Product\Controllers\ProductController@stor
 
 Route::get('/category', '\App\Modules\Category\Controllers\CategoryController@index')->name('category.index')->middleware('auth');
 Route::get('/addCategory/{category_id?}', '\App\Modules\Category\Controllers\CategoryController@getCategory')->name('addCategory')->middleware('auth');
-Route::post('/addCategory/{category_id?}', '\App\Modules\Category\Controllers\CategoryController@postCategory')->name('postCategory')->middleware('auth');
+Route::post('/addCategory/{category_id?}', '\App\Modules\Category\Controllers\CategoryController@postCategory')->middleware('auth');
+Route::get('category/show/{id}', '\App\Modules\Category\Controllers\CategoryController@show')->name('category.show')->middleware('auth');
 
 //payment method route
 Route::get('/payment_method', '\App\Modules\PaymentMethod\Controllers\PaymentMethodController@index')->name('payment_method.index')->middleware('auth');
 Route::get('/payment_method/create', '\App\Modules\PaymentMethod\Controllers\PaymentMethodController@create')->name('payment_method.create')->middleware('auth');
+Route::get('/payment_method/{id}', '\App\Modules\PaymentMethod\Controllers\PaymentMethodController@show')->name('payment_method.show')->middleware('auth');
 //order method route
 Route::get('/order', '\App\Modules\Order\Controllers\OrderController@index')->name('order.index')->middleware('auth');
 //brand method route
