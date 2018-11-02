6b72c0a133f50fbf342bebc3a369215c7ca31554
shriyafxbytes <jain.shriya@fxbytes.com>
2018-11-02 10:57:05 +0530

    script to calculate discount for product added

--- app/Modules/Product/Views/create.blade.php
+++ app/Modules/Product/Views/create.blade.php
@@ -35,7 +35,7 @@
 				</div>
 				<!-- Mobile Number Field -->
 				<div class="form-group">
-					<input type="text" name="product_selling_price" value="" id="product_selling_price" class="form-control input-lg" maxlength="100" placeholder="product selling price" tabindex="3"  />
+					<input type="text" name="product_selling_price" value="" id="product_selling_price" class="form-control input-lg" maxlength="100" placeholder="product selling price" tabindex="3" disabled="" readonly="" />
 				</div>
 				<div class="form-group">
 					<label for="category_name" class="sr-only">Category Name</label>
@@ -58,4 +58,29 @@
 </div>
 @endsection
 @section('scripts')
+<script type="text/javascript">
+$(document).on("change keyup blur", "#product_discount", function() {
+  var amd = $('#product_price').val();
+  var disc = $('#product_discount').val();
+  if ( $.isNumeric(disc) && disc != '' && $.isNumeric(amd) && amd != '') {
+  	var discount =(amd*disc)/100;
+
+    $('#product_selling_price').val((parseFloat(amd)) - (parseFloat(discount)));
+  }else{
+    $('#product_selling_price').val('Please Enter Price And Discount Value In Numeric Form');
+  }
+});
+$(document).on("change keyup blur", "#product_price", function() {
+  var amd = $('#product_price').val();
+  var disc = $('#product_discount').val();
+  if ( $.isNumeric(disc) && disc != '' && $.isNumeric(amd) && amd != '') {
+  	var discount =(amd*disc)/100;
+
+    $('#product_selling_price').val((parseFloat(amd)) - (parseFloat(discount)));
+  }else{
+    $('#product_selling_price').val('Please Enter Price And Discount Value In Numeric Form');
+  }
+});
+
+</script>
 @endsection
